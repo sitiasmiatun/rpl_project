c11aea94af695967c9743ebcd1de9028a497c2ca
rudinndeso <mailipunrudin@gmail.com>
2025-11-17 11:12:52 +0700

    deploy ably

--- rpl_project/settings.py
+++ rpl_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-x!$+r%kcqb@sjz!7b)guzmgm81j9+%le$pg3s2crx6j9^17mck
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']  # atau nama domain render/railway Anda
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
