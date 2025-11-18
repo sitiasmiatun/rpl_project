add78e5a30e13ed13d031b918ffacc091496bf89
rudinndeso <mailipunrudin@gmail.com>
2025-11-18 10:38:30 +0700

    Tambah homepage

--- rpl_project/urls.py
+++ rpl_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('mahasiswa/', include('mahasiswa.urls')),
 path('accounts/', include('accounts.urls')),
+path('', RedirectView.as_view(url='/mahasiswa/')),
 ]
