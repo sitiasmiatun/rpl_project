a6ed2767dda9b9038009b0e52139ac2c47f0e0b2
rudinndeso <mailipunrudin@gmail.com>
2025-11-20 10:08:13 +0700

    update project

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('daftar/', views.daftar_mahasiswa, name='daftar_mahasiswa'),
     path('tambah/', views.tambah_mahasiswa, name='tambah_mahasiswa'),
     path('edit/<int:id>/', views.edit_mahasiswa, name='edit_mahasiswa'),
     path('hapus/<int:id>/', views.hapus_mahasiswa, name='hapus_mahasiswa'),
+    path('', RedirectView.as_view(url='daftar/')),
 ]
